cece848a7153ec2dda60ca99c1f558821a054f47
Aniket Kumar <cs5160398@cse.iitd.ac.in>
2018-03-20 20:15:04 +0530

    done

--- find-malicious.py
+++ find-malicious.py
@@ -1,13 +1,28 @@
 import requests
 from bs4 import BeautifulSoup 
 from gglsbl import SafeBrowsingList
-import yaml
+import yaml, sys
 
-def find_link(link):
+
+def find_links(link):
 	page = requests.get(link)
-	page.raise_for_status()
+	try:
+		page.raise_for_status()
+	except:
+		return []
 	soup = BeautifulSoup(page.text, "lxml")
-	links = soup.findAll('a')
+	elements = soup.findAll('a')
+	links = []
+	for a in elements:
+		my_link = a.get('href')
+		if (my_link==None or len(my_link)==0 or my_link=='#'):
+			continue
+		elif (my_link[0]=='/'):
+			links.append(link + my_link)
+		elif (my_link[0]!='h'):
+			links.append(link + '/' + my_link)
+		else:
+			links.append(my_link)
 	return links
 
 
@@ -22,10 +37,23 @@ def find_threats(link, api_key):
 
 
 def main():
+	threats = {}
 	with open('keys.yaml') as f:
 		keys = yaml.load(f)
 		key = keys['API_KEY']
-	print (find_threats("http://www.doubletree3hilton.com", key))
+
+	filename = sys.argv[1]
+	file_object = open(filename, "r")
+	for line in file_object:
+		# print (line)
+		link = "https://www.cse.iitd.ac.in/~"+line[:-1]
+		print (link)
+		for web_link in find_links(link):
+			print (web_link)
+			threats[web_link] = find_threats(web_link, key)
+
+	print (threats)
+
 
 if __name__=='__main__':
     main()
